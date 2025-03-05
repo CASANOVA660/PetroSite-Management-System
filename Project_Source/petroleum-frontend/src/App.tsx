afa3154005599608f7c3c18b75106673bdc8477c
Mohamed Amin Bouali <boualiamino0123@gmail.com>
2025-03-05 14:20:01 +0100

    complete the login feat n the frontend

--- Project_Source/petroleum-frontend/src/App.tsx
+++ Project_Source/petroleum-frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import { ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App = (): ReactElement => {
-  return <Outlet />;
+  return (
+    <Provider store={store}>
+      <Outlet />
+    </Provider>
+  );
 };
 
 export default App;
